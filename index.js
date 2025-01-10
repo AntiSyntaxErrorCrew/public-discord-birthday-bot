694e4f8fcb2a7c72e9172f7d0f38a73bbe221be2
Kenny Zhang <kennyzhang418@gmail.com>
2025-01-10 11:23:34 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const { MongoClient } = require("mongodb");
 const { Client, IntentsBitField } = require("discord.js");
 
 const client = new Client({
@@ -10,55 +11,75 @@ const client = new Client({
   ],
 });
 
-// edit
-const channelId = "";
+const uri = "" // your mongodb uri
+const mongoClient = new MongoClient(uri);
 
+const channelId = "" // your channel id;
+let startTime;
 const currentYear = new Date().getFullYear();
 const nextYear = currentYear + 1;
 
-// edit
-const birthdayData = [                       
-  { date: "01/1/2024, 12:00:00 AM", userId: "" },
-];
-
-let startTime;
-
-function updateBirthdayYears() {
-  const now = new Date();
-  birthdayData.forEach((birthdayObj) => {
-    let birthday = new Date(birthdayObj.date);
-
-    // Check if the birthday has already occurred this year
-    if (now.getFullYear() === birthday.getFullYear()) {
-      if (now.getTime() > birthday.getTime()) {
-        // Move birthday to next year
-        birthday.setFullYear(nextYear);
-        birthdayObj.date = birthday.toLocaleString();
-      }
-    }
-  });
+async function connectDB() {
+  try {
+    await mongoClient.connect();
+    const db = mongoClient.db("personal");
+    const collection = db.collection("discord-birthdays");
+    return collection;
+  } catch (error) {
+    console.error("error connecting to mongodb:", error);
+  }
 }
 
-// Update birthday years initially
-updateBirthdayYears();
-console.log(birthdayData);
+async function updateBirthdayYears() {
+  const collection = await connectDB();
+  const now = new Date();
 
-client.on("ready", (message) => {
-  console.log(`Logged in as ${message.user.tag}`);
+  try {
+    const birthdays = await collection.find().toArray();
+
+    const updates = birthdays.map((birthdayObj) => {
+      let birthday = new Date(birthdayObj.date);
+      if (
+        now.getFullYear() === birthday.getFullYear() &&
+        now.getTime() > birthday.getTime()
+      ) {
+        birthday.setFullYear(nextYear);
+        return collection.updateOne(
+          { _id: birthdayObj._id },
+          { $set: { date: birthday.toLocaleString() } }
+        );
+      }
+    });
+
+    await Promise.all(updates);
+    console.log("updated birthdays");
+  } catch (error) {
+    console.error("error updating birthdays:", error);
+  }
+}
+updateBirthdayYears();
+
+client.on("ready", async () => {
+  console.log(`Logged in as ${client.user.tag}`);
 
   const channel = client.channels.cache.get(channelId);
 
-  // Check birthdays every 10 seconds
-  setInterval(() => {
+  setInterval(async () => {
     const now = new Date();
-    birthdayData.forEach((birthdayObj) => {
+    const collection = await connectDB();
+    const birthdays = await collection.find().toArray();
+
+    birthdays.forEach(async (birthdayObj) => {
       let birthday = new Date(birthdayObj.date);
       if (now.getTime() > birthday.getTime()) {
         channel
-          .send(`Happy birthday <@${birthdayObj.userId}> 🎉`)
+          .send(`Happy birthday <@${birthdayObj._id}> 🎉`)
           .catch(console.error);
         birthday.setFullYear(nextYear);
-        birthdayObj.date = birthday.toLocaleString();
+        await collection.updateOne(
+          { _id: birthdayObj._id },
+          { $set: { date: birthday.toLocaleString() } }
+        );
       }
     });
   }, 10000);
